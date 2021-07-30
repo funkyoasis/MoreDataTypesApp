14ecbd1186573ede771af3f98820e31931e6c1f4
arthur tshaka <arthurltshaka@gmail.com>
2021-07-30 12:53:36 +0100

    first commit

---
+++ MoreDataTypesApp/MoreDataTypesApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MoreDataTypesApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var myString = " c# list fundamentals ";
+			Console.WriteLine(StringExcercise(myString));
+		}
+		public static string StringExcercise(string myString)
+		{
+			var s1 = myString.Trim().ToUpper().Replace("L","*").Replace("T", "*");
+			int index = s1.IndexOf('N');
+			var finalString = s1.Remove(index + 1);
+			return finalString;
+
+		}
+	}
+}
